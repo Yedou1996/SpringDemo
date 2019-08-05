9ab9f84a2f1df5ebfd1d45787db9adc6ebbcd813
LXL <328161507@qq.com>
2019-08-05 16:28:25 +0800

    git up config code to properties

--- src/main/java/com/lxl/demo/community/controller/AuthorizeController.java
+++ src/main/java/com/lxl/demo/community/controller/AuthorizeController.java
@@ -4,6 +4,7 @@ import com.lxl.demo.community.dto.AccessTokenDTO;
 import com.lxl.demo.community.dto.GitHupUser;
 import com.lxl.demo.community.provider.GitHupProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,15 +13,21 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AuthorizeController {
     @Autowired
     private GitHupProvider gitHupProvider;
+    @Value("${gitHup.client.id}")
+    private String clientId;
+    @Value("${gitHup.client.secret}")
+    private String clientSecret;
+    @Value("${gitHup.redirect.uri}")
+    private String redirectUri;
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code")String code,
                            @RequestParam(name="state")String state
                            ){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("77e29ec87ed5c314b1e5");
-        accessTokenDTO.setClient_secret("0d0f793514d17edb1cb956d5b756e71b41f86112");
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
         accessTokenDTO.setCode(code);
-        accessTokenDTO.setRedirect_uri("http://localhost:8887/callback");
+        accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = gitHupProvider.getAccessToken(accessTokenDTO);
         GitHupUser user = gitHupProvider.getUser(accessToken);
